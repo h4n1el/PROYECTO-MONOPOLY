b39c2bd3ac4759553b4b8e0b134e971ae00dc7ef
h4n1el <66563648+h4n1el@users.noreply.github.com>
2026-08-07 23:35:36 -0400

    Fix class name from CartaMovimiento to CartaMovidaPor
    arregla nombre del constructor

--- Monopolio/src/model/mazo/CartaMovidaPor.java
+++ Monopolio/src/model/mazo/CartaMovidaPor.java
@@ -2,7 +2,7 @@ package Monopolio.gamedata.mazo.CartaMovidaPor;
 public class CartaMovidaPor extends Cartas{
     private final int distancia;
 
-    public CartaMovimiento(String descripcion, boolean fromMazoCartas, int distancia)
+    public CartaMovidaPor(String descripcion, boolean fromMazoCartas, int distancia)
     {
         super(descripcion, fromMazoCartas);
         this.distancia = distancia;

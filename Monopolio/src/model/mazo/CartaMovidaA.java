954d97c319f0309dd6d5a1dfa7a58f8fc4cb88d5
h4n1el <66563648+h4n1el@users.noreply.github.com>
2026-08-07 23:34:27 -0400

    Update CartaMovidaA.java
    correcciones en la propiedad ubicacion y en la condicion

--- Monopolio/src/model/mazo/CartaMovidaA.java
+++ Monopolio/src/model/mazo/CartaMovidaA.java
@@ -1,17 +1,17 @@
-package Monopolio.gamedata.mazo.CartaMovidaA;
+package model.mazo;
 public class CartaMovidaA extends Cartas
 {
     private final int moverUbicacion;
 
-    public CartaMovidaA(String descripcion, boolean fromMazoCartas, int moverUbicacion)
+    public CartaMovidaA(String descripcion, boolean fromMazoCartas, int ubicacion)
     {
         super(descripcion, fromMazoCartas);
-        if(moverUbicacion > 39){
+        if(ubicacion > 39){
             this.moverUbicacion = 39;
         }else if(ubicacion < 0){
             this.moverUbicacion = 0;
         }else {
-            this.moverUbicacion = ubicacion
+            this.moverUbicacion = ubicacion;
         }
         
     }
